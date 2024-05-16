9986e828daff96fae261c754c3e079f24493e55c
Pakorn Chan-in <hnong2004@hotmail.com>
2024-05-16 16:33:23 +0700

    counting

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My increment counter"),
+      ),
+      body: Center(
+        child: Text("นัยไปแล้ว $_counter ครั้ง"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,        
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
